37b8cfa4999d0de20fa27fd22e57f4618fb696f3
Xie Yanbo <xieyanbo@gmail.com>
2023-12-28 16:38:18 +0800

    fix: authuser is not compatible with Django admin

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,6 +1,4 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .models import User
-
-admin.site.register(User, UserAdmin)
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+# admin.site.register(User, UserAdmin)
