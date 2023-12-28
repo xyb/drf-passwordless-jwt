0200d1dccd7c6c8bf7919b98ac5ae61bbf283789
Xie Yanbo <xieyanbo@gmail.com>
2023-12-28 16:21:08 +0800

    fix: authuser models

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,1 +1,6 @@
-# admin.site.register(User, UserAdmin)
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+admin.site.register(User, UserAdmin)
--- authuser/tests.py
+++ authuser/tests.py
@@ -1,1 +1,29 @@
-# Create your tests here.
+import pytest
+from django.contrib.auth.models import User as DjangoUser
+from django.test import TestCase
+
+from .models import User
+
+
+class UserManagerTestCase(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user("xyb@test.com", "password")
+
+        self.assertTrue(isinstance(user, User))
+
+    def test_create_user_no_email(self):
+        with pytest.raises(ValueError) as excinfo:
+            User.objects.create_user("")
+
+        assert str(excinfo.value) == "The given email must be set"
+
+    def test_create_super_user(self):
+        user = User.objects.create_superuser("xyb@test.com", "password")
+
+        self.assertTrue(isinstance(user, User))
+
+    @pytest.mark.xfail
+    def test_create_user_django(self):
+        user = DjangoUser.objects.create_user("xyb@test.com", "password")
+
+        self.assertTrue(isinstance(user, User))
